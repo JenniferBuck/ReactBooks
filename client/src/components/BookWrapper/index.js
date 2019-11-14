ceeb8c761a02ae51063ecca3ed40a2c8fb20e842
Fred Saba <fred@fred-saba.com>
2019-11-14 11:50:35 -0800

    experimenting with hooks

--- client/src/components/BookWrapper/index.js
+++ client/src/components/BookWrapper/index.js
@@ -1,22 +1,17 @@
-import React,{Component} from 'react'
+import React,{useState} from 'react'
 import './style.css'
 import API from '../../api'
 
-class BookWrapper extends Component {
+const BookWrapper = props => {
 
-    state = {
-        message : ""
-    }
-
-    saveArticle = book => {
+    const [message, setMessage] = useState("")
+    
+    const saveArticle = book => {
         API.saveBook(book)
-        this.setState({message:"This Book Has Been Saved! "})
-
+        setMessage("Your book has been saved")
     }
 
-
-render(){
-    const {authors,title,description,link,image} = this.props
+    const {authors,title,description,link,image} = props
   
     return(
         <div className="book-wrapper">
@@ -29,13 +24,53 @@ render(){
             
             <h2><img alt={title +"image"} src={image}></img></h2>
             <h2><a className="link" rel="noopener noreferrer" target="_blank" href={link}>Link to Book</a></h2>
-            <h2><button className="save-btn" onClick={()=> this.saveArticle(this.props)}>Save Book</button></h2>
-            <h2>{this.state.message}</h2>
+            <h2><button className="save-btn" onClick={()=> saveArticle(props)}>Save Book</button></h2>
+            <h2>{message}</h2>
             </div>
         </div>
     )
-}
+
+
+
 
 }
+
+// class BookWrapper extends Component {
+
+//     state = {
+//         message : ""
+//     }
+
+//     saveArticle = book => {
+//         API.saveBook(book)
+//         this.setState({message:"This Book Has Been Saved! "})
+
+//     }
+
+
+// render(){
+//     const {authors,title,description,link,image} = this.props
+  
+//     return(
+//         <div className="book-wrapper">
+//             <div>
+//             <h1>{title} Written By : {authors.join(", ")}</h1>
+//             <h3>Synopsis : {description}</h3>
+           
+//             </div>
+//             <div>
+            
+//             <h2><img alt={title +"image"} src={image}></img></h2>
+//             <h2><a className="link" rel="noopener noreferrer" target="_blank" href={link}>Link to Book</a></h2>
+//             <h2><button className="save-btn" onClick={()=> this.saveArticle(this.props)}>Save Book</button></h2>
+//             <h2>{this.state.message}</h2>
+//             </div>
+//         </div>
+//     )
+// }
+
+// }
+
+
 
 export default BookWrapper
--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react'
+import React,{useState} from 'react'
 import SearchWrapper from '../../components/SearchWrapper'
 import SearchResultsWrapper from '../../components/SearchResultsWrapper'
 import NoResults from '../../components/NoResults'
@@ -6,27 +6,26 @@ import SavedBookWrapper from '../../components/SavedBookWrapper'
 import API from '../../api/index'
 import './style.css';
 
-class Saved extends Component{
 
-    state = {
-        savedbooks: []
+const Saved = () => {
+
+    const [books,setBooks] = useState([])
+
+        const componentDidMount = () => {
+        findSavedBooks()
     }
 
-    componentDidMount = () => {
-        this.findSavedBooks()
-    }
-
-    findSavedBooks = async () =>{
-        let {data:savedBooks} = await API.findSavedBooks()
-        this.setState({savedbooks:savedBooks},()=>console.log(this.state.savedbooks))
+    const findSavedBooks = async () =>{
+        const {data:savedBooks} = await API.findSavedBooks()
+        setBooks(savedBooks)
     } 
 
-    handleClick = async id => {
+    const handleClick = async id => {
         await API.deleteSavedBook(id)
-        this.findSavedBooks()     
+        findSavedBooks()     
     }  
+    componentDidMount()
 
-    render(){
         return(
             <SearchWrapper>
             <div className="header-wrapper">
@@ -35,9 +34,9 @@ class Saved extends Component{
             
         
              <SearchResultsWrapper>
-                {this.state.savedbooks && this.state.savedbooks.length > 0 ? 
+                {books && books.length > 0 ? 
                 
-                this.state.savedbooks.map(book =>(
+                books.map(book =>(
                     <SavedBookWrapper
                     key={book._id}
                     _id={book._id}
@@ -46,7 +45,7 @@ class Saved extends Component{
                     description={book.description ? book.description : "No Description Available"}
                     link={book.link}
                     image={book.image ? book.image : "#"}
-                    handleClick={this.handleClick}
+                    handleClick={handleClick}
                    
                      />
                 ))
@@ -59,7 +58,51 @@ class Saved extends Component{
             </SearchWrapper>
             
         )
-    }
+
 }
+
+// class Saved extends Component{
+
+//     state = {
+//         savedbooks: []
+//     }
+
+
+
+//     render(){
+//         return(
+//             <SearchWrapper>
+//             <div className="header-wrapper">
+//             <h2 className="header">Saved Books</h2>
+//             </div>
+            
+        
+//              <SearchResultsWrapper>
+//                 {this.state.savedbooks && this.state.savedbooks.length > 0 ? 
+                
+//                 this.state.savedbooks.map(book =>(
+//                     <SavedBookWrapper
+//                     key={book._id}
+//                     _id={book._id}
+//                     authors={book.authors ? book.authors : ["No Author Available"]}
+//                     title={book.title}
+//                     description={book.description ? book.description : "No Description Available"}
+//                     link={book.link}
+//                     image={book.image ? book.image : "#"}
+//                     handleClick={this.handleClick}
+                   
+//                      />
+//                 ))
+                
+//                 : <NoResults />}
+                 
+//             </SearchResultsWrapper>   
+      
+
+//             </SearchWrapper>
+            
+//         )
+//     }
+// }
 
 export default Saved
